92a95c20a59e6026e0a6fcd1740f1f4107ca4f79
Chew Han Xiang <5371541+chewhx@users.noreply.github.com>
2023-11-01 11:28:46 +0800

    :sparkles: (src/lib) add author and isbn functions to export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { search } from './lib/search';
 export { id } from './lib/id';
 export { title } from './lib/title';
+export { author } from './lib/author';
+export { isbn } from './lib/isbn';
